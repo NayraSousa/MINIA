73efbaa6f8f70d0b93d60622cc7d93e9cc3bffb6
NayraSousa <nayraamaral1@gmail.com>
2026-04-06 16:41:07 -0300

    refactor: trocar nome da tabela application

---
+++ backend/src/app/databases/migrations/20260406193447_job_application.js
@@ -0,0 +1,33 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('job_application', function(table){
+        table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+        table.uuid('candidate_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('candidate');
+        table.uuid('job_id')
+            .unsigned()
+            .notNullable()
+            .unique()
+            .references('id')
+            .inTable('job');
+        table.string('status').notNullable();
+        table.double('ai_score');
+        table.dateTime('created_at');
+        
+    })
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('job_application');
+};
